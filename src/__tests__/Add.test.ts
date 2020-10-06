b650bccf841f3015b3671d16d2dd99935863909e
Anshul Dharmadhikari <anshul.d@talview.com>
2020-10-06 19:04:54 +0530

    1.0.1

--- src/__tests__/Add.test.ts
+++ src/__tests__/Add.test.ts
@@ -4,17 +4,25 @@
 
 import Add from '../index';
 test('Adds entry to AlphaOne', async () => {
-    expect(await Add('2020-10-06T08:57:12.096Z', '2020-10-06T11:57:16.096Z', 'AlphaOne')).toBe('Entry added to system AlphaOne');
+  expect(await Add('2020-10-06T08:57:12.096Z', '2020-10-06T11:57:16.096Z', 'AlphaOne')).toBe(
+    'Entry added to system AlphaOne',
+  );
 });
 
 test('Adds entry to AlphaTwo', async () => {
-    expect(await Add('2020-10-06T10:57:12.096Z', '2020-10-06T11:57:16.096Z', 'AlphaTwo')).toBe('Entry added to system AlphaTwo');
+  expect(await Add('2020-10-06T10:57:12.096Z', '2020-10-06T11:57:16.096Z', 'AlphaTwo')).toBe(
+    'Entry added to system AlphaTwo',
+  );
 });
 
 test('end time cannot be less than start time', async () => {
-    expect(await Add('2020-10-06T11:57:16.096Z', '2020-10-06T11:57:12.096Z', 'AlphaTwo')).toBe('endTime cannot be less than startTime');
+  expect(await Add('2020-10-06T11:57:16.096Z', '2020-10-06T11:57:12.096Z', 'AlphaTwo')).toBe(
+    'endTime cannot be less than startTime',
+  );
 });
 
 test('invalid system value', async () => {
-    expect(await Add('2020-10-06T11:57:16.096Z', '2020-10-06T21:57:12.096Z', 'default')).toBe('Entry added to system default');
+  expect(await Add('2020-10-06T11:57:16.096Z', '2020-10-06T21:57:12.096Z', 'default')).toBe(
+    'Entry added to system default',
+  );
 });
